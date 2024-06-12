54763449e3f572c4b260695cb0ae43b4981ad638
Mahmoud-Adel-Ali <mahmoudadela413@gmail.com>
2024-06-11 19:50:49 -0700

    add desktop layout

--- lib/widgets/home_view_body.dart
+++ lib/widgets/home_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:adaptive_app/widgets/desktop_layout.dart';
 import 'package:adaptive_app/widgets/mobile_layout.dart';
 import 'package:adaptive_app/widgets/tablet_layout.dart';
 import 'package:flutter/material.dart';
@@ -7,18 +8,17 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Padding(
-      padding: EdgeInsets.only(left: 16,right: 16,top: 4,bottom: 4),
-      child: LayoutBuilder(
-        builder: (context,constrains) {
-          if (constrains.maxWidth > 500) {
-            return const TabletLayout();
-          } else {
-            return const MobileLayout();
-          }
+    return Padding(
+      padding: const EdgeInsets.only(left: 16, right: 16, top: 4, bottom: 4),
+      child: LayoutBuilder(builder: (context, constrains) {
+        if (constrains.maxWidth > 900) {
+          return const DesktopLayout();
+        } else if (constrains.maxWidth > 500) {
+          return const TabletLayout();
+        } else {
+          return const MobileLayout();
         }
-      ),
+      }),
     );
   }
 }
-

431e02112d6f25bc9d014967abcad6b49341bc77
Mahmoud-Adel-Ali <mahmoudadela413@gmail.com>
2024-06-11 22:15:11 -0700

    hidden app bar on desktop layout

--- lib/screens/home_view.dart
+++ lib/screens/home_view.dart
@@ -1,3 +1,4 @@
+
 import 'package:adaptive_app/widgets/custom_drawer.dart';
 import 'package:adaptive_app/widgets/home_view_body.dart';
 import 'package:flutter/material.dart';
@@ -17,18 +18,21 @@ class _HomeViewState extends State<HomeView> {
       key: scaffoldKey,
       drawer: const CustomDrawer(),
       backgroundColor: const Color(0xffDFDFDF),
-      appBar: AppBar(
-        backgroundColor: Colors.black,
-        leading: IconButton(
-            onPressed: () {
-              scaffoldKey.currentState!.openDrawer();
-            },
-            icon: const Icon(
-              Icons.menu,
-              size: 28,
-              color: Colors.white,
-            )),
-      ),
+      appBar: MediaQuery.of(context).size.width <= 932
+          ? AppBar(
+              backgroundColor: Colors.black,
+              leading: IconButton(
+                onPressed: () {
+                  scaffoldKey.currentState!.openDrawer();
+                },
+                icon: const Icon(
+                  Icons.menu,
+                  size: 28,
+                  color: Colors.white,
+                ),
+              ),
+            )
+          : null,
       body: const HomeViewBody(),
     );
   }
--- lib/widgets/home_view_body.dart
+++ lib/widgets/home_view_body.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:adaptive_app/widgets/desktop_layout.dart';
 import 'package:adaptive_app/widgets/mobile_layout.dart';
 import 'package:adaptive_app/widgets/tablet_layout.dart';
@@ -11,6 +13,8 @@ class HomeViewBody extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.only(left: 16, right: 16, top: 4, bottom: 4),
       child: LayoutBuilder(builder: (context, constrains) {
+        log("media query width = ${MediaQuery.of(context).size.width.toString()}");
+        log("layout width = ${constrains.maxWidth.toString()}");
         if (constrains.maxWidth > 900) {
           return const DesktopLayout();
         } else if (constrains.maxWidth > 500) {
